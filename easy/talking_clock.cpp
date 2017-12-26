66c8d681d3269ee3fa849946905ef8d796c64196
Vladislav Ligai <vligai@stevens.edu>
2017-12-25 19:31:57 -0500

    Update talking_clock.cpp

--- easy/talking_clock.cpp
+++ easy/talking_clock.cpp
@@ -1,4 +1,6 @@
 /*
+[2017-06-27] Challenge #321
+
 Description
 No more hiding from your alarm clock! You've decided you want your computer to keep you updated on the time 
 so you're never late again. A talking clock takes a 24-hour time and translates it into words.
